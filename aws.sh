cc19abc4a95b7ab43f73500c2dc55dc848c02ac5
Stephanie Frantz <stephanie.frantz@cengage.com>
2022-03-04 19:25:08 -0500

    added some more stuff

--- aws.sh
+++ aws.sh
@@ -3,6 +3,13 @@
 echo "Getting ready..."
 rm -rf /tmp/cloudsploit
 
+if ! [ -x "$(command -v git)" ]; then
+        echo "The git command is required! We're going to install it for you... soo you might get asked for sudo."
+        sudo apt install git-all
+        echo "git has been installed!"
+fi
+
+
 mkdir /tmp/cloudsploit
 cd /tmp/cloudsploit
 git clone git@github.com:cloudsploit/scans.git
@@ -10,28 +17,37 @@ cd scans
 npm install --silent
 chmod +x index.js
 
+
+## The cool thing to do would be READ and 
+## throw it into a case statement block..
+
+###########################################
+############### AWS BLOCK #################
+###########################################
+CLOUD_TYPE="aws"
+
 echo "Please enter the AWS ACCESS KEY ID:"
 echo -e "AWS_ACCESS_KEY_ID: "
 read AWS_ACCESS_KEY_ID
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 
 echo "Please enter the AWS SECRET ACCESS KEY"
 echo -e "AWS_SECRET_ACCESS_KEY: "
 read AWS_SECRET_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
 echo "What is the region that you're located primarily (example: us-east-1)?"
 echo -e "AWS Region: "
 read AWSREGION
-
-
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_REGION=${AWSREGION}
+
+###########################################
 
 echo "Where do you want the csv exported..? (example: /Users/username/report.csv ) \n"
 echo -e "File Location: "
 read FILE_LOCATION
 
-"./index.js" --cloud aws --csv $FILE_LOCATION --console none
+"./index.js" --cloud $CLOUD_TYPE --csv $FILE_LOCATION --console none
 echo -e "\n\n\n\n"
 echo "Please see $FILE_LOCATION for your CSV output!"
 
